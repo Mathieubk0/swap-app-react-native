82cfe1400a93beb6a144294d06cfbed3864c6d83
Matheus <MathieuBk0@gmail.com>
2024-01-17 15:48:13 +0000

    Translated Form logic to React Native. Still needs to handle  DATE multiple submissions

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
 
 import { format } from 'date-fns';
-import ToastManager, { Toast } from 'toastify-react-native';
+import ToastManager from 'toastify-react-native';
 import Calendar from './components/Calendar/Calendar';
 import InlineForm from './components/InlineForm/InlineForm';
 
@@ -18,8 +18,6 @@ export default function App() {
     return day < todayDate;
   };
 
-  const [shifts, setShifts] = useState([{isOvernight: false, Date: '', Outbound: '', Inbound: '', Position:'', Early: false, Late: false, LTA: false, DO: false}]);
-
   const [showQuickView, setShowQuickView] = useState(false);
   const toggleQuickViewBox = () => {
     setShowQuickView(!showQuickView);
@@ -30,90 +28,16 @@ export default function App() {
     setSelectedDay(prevSelectedDay => (prevSelectedDay && prevSelectedDay.getTime() === day.getTime() ? null : day));
   };
 
-  const addShift = () => {
-    const newShifts = [...shifts, {isOvernight: false, Date: '', Outbound: '', Inbound: '', Position:'', Early: false, Late: false, LTA: false, DO: false}];
-    setShifts(newShifts)
-  };
-
-  const deleteShift = (index) => {
-    const updatedShifts = [...shifts];
-    updatedShifts.splice(index, 1);
-    setShifts(updatedShifts);
-  };
-
-  const ovSwitch = (index) => { 
-    const updatedShifts = [...shifts];
-    updatedShifts[index].isOvernight = !updatedShifts[index].isOvernight;
-    setShifts(updatedShifts)
-  };
-
-  const handleChange = (index, fieldName, value) => {
-    const updatedShifts = [...shifts];
-    updatedShifts[index][fieldName] = value;
-    setShifts(updatedShifts);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const isAnyOutdated = shifts.some(shift => shift.Date && isOutdated(new Date(shift.Date)));
-
-    if (isAnyOutdated) {
-      Toast.error('Oops... You can\'t submit an outdated swap 🤓');
-      return;
-    }
-
-    shifts.forEach(shift => {
-      const formData = {
-        Email: e.target.elements.Email.value,
-        Date: shift.Date,
-        Outbound: shift.Outbound,
-        Inbound: shift.isOvernight ? shift.Inbound + '+1d' : shift.Inbound,
-        Position: shift.Position,
-        Early: shift.Early,
-        Late: shift.Late,
-        LTA: shift.LTA,
-        DO: shift.DO,
-        Note: e.target.elements.Note.value
-      };
-
-      fetch(`${BASEURL}/formData`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(formData)
-        })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Form submission failed');
-        }
-        return response.json()
-      })
-      .then(data => {
-        console.log('Success', data);
-        Toast.success(`${shift.Outbound} - ${shift.Inbound} on ${shift.Date} submitted successfully!`);
-        // setShowQuickView(true)
-      })
-      .catch(error => {
-        console.log(error);
-        Toast.error(`${shift.Outbound} - ${shift.Inbound} on ${shift.Date} submission failed`)
-      });
-    });
-  };
-
-  const showToasts = () => {
-    Toast.success('Youhou! Let\'s GO!')
-  }
-
   return (
     <View style={styles.container}>
       <ScrollView>
         <View style={styles.display}>
-          <Text style={styles.greetings} onPress={showToasts}>Hi there! Today is { format(todayDate, 'MMMM do, y O') }</Text>
+          <Text style={styles.greetings}>Hi there! Today is { format(todayDate, 'MMMM do, y O') }</Text>
           <View style={styles.InlineForm}>
-            <InlineForm BASEURL= {BASEURL} todayDate={todayDate} isOutdated={isOutdated} addShift={addShift} deleteShift={deleteShift} ovSwitch={ovSwitch} handleChange={handleChange} shifts={shifts} handleSubmit={handleSubmit} />
+            <InlineForm BASEURL= {BASEURL} isOutdated={isOutdated} />
           </View>
           <View style={styles.Calendar}>
-            <Calendar  showToasts={showToasts} BASEURL= {BASEURL} todayDate={todayDate}isOutdated={isOutdated} showQuickView={showQuickView} toggleQuickViewBox={toggleQuickViewBox} selectedDay={selectedDay} toggleDayBox={toggleDayBox} />
+            <Calendar BASEURL= {BASEURL} isOutdated={isOutdated} showQuickView={showQuickView} toggleQuickViewBox={toggleQuickViewBox} selectedDay={selectedDay} toggleDayBox={toggleDayBox} />
           </View>
         </View>
       </ScrollView>
--- components/InlineForm/InlineForm.js
+++ components/InlineForm/InlineForm.js
@@ -1,66 +1,203 @@
-import React from "react";
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput, Switch, Checkbox, Linking } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput, Switch, Linking } from 'react-native';
+import CheckBox from "expo-checkbox";
+import { Toast } from 'toastify-react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { format } from 'date-fns';
 
-export default function InlineForm({ addShift, deleteShift, ovSwitch, handleChange, handleSubmit, shifts }) {
+export default function InlineForm({ BASEURL, isOutdated }) {
   
-  const handleRosterPress = () => { Linking.openURL('https://www.momentumserviceslondon.com/activite')};
-  const handleSwapFormPress = () => { Linking.openURL('https://app.smartsheet.com/b/form/20d18963576e477bafcbf102df2aec3d')};
+  const handleLink1Press = () => { Linking.openURL('https://www.google.com')};
+  const handleLink2Press = () => { Linking.openURL('https://www.wikipedia.org')};
+
+  const [shifts, setShifts] = useState([{isOvernight: false, Date: '', Outbound: '', Inbound: '', Position:'', Early: false, Late: false, LTA: false, DO: false}]);
+
+  const handleChange = (index, fieldName, value) => {
+    const updatedShifts = [...shifts];
+    updatedShifts[index][fieldName] = value;
+    setShifts(updatedShifts);
+  };
+
+  const addShift = () => {
+    const newShifts = [...shifts, {isOvernight: false, Date: '', Outbound: '', Inbound: '', Position:'', Early: false, Late: false, LTA: false, DO: false}];
+    setShifts(newShifts)
+  };
+
+  const deleteShift = (index) => {
+    const updatedShifts = [...shifts];
+    updatedShifts.splice(index, 1);
+    setShifts(updatedShifts);
+  };
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  const handleDateChange = (event, date) => {
+    setShowDatePicker(false);
+    if (date) {
+      setSelectedDate(date);
+    }
+  };
+  const formatDate = (date) => {
+    return format(date, 'dd/MM/yyyy');
+  };
+  const openDatePicker = () => {
+    setShowDatePicker(true);
+  };
+
+  const [email, onChangeEmail] = useState('');
+  const [note, onChangeNote] = useState('');
+  const [isOvernight, setIsOvernight] = useState(false);
+
+  const ovSwitch = (index) => { 
+    return () => {
+      setIsOvernight((previousState) => !previousState);
+
+      const updatedShifts = [...shifts];
+      updatedShifts[index].isOvernight = !updatedShifts[index].isOvernight;
+      setShifts(updatedShifts);
+    };
+  };
+  const [selectedPosition, setSelectedPosition] = useState(null);
+
+  const handleSubmit = () => {
+
+    const isAnyOutdated = shifts.some(shift => shift.Date && isOutdated(new Date(shift.Date)));
+
+    if (isAnyOutdated) {
+      Toast.error('Oops... You can\'t submit an outdated swap 🤓');
+      return;
+    }
+
+    shifts.forEach(shift => {
+      const formData = {
+        Email: email,
+        Date: formatDate(selectedDate),
+        Outbound: shift.Outbound,
+        Inbound: shift.isOvernight ? shift.Inbound + '+1d' : shift.Inbound,
+        Position: selectedPosition,
+        Early: shift.Early,
+        Late: shift.Late,
+        LTA: shift.LTA,
+        DO: shift.DO,
+        Note: note
+      };
+    
+      fetch(`${BASEURL}/formData`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData)
+        })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Form submission failed');
+        }
+        return response.json()
+      })
+      .then(data => {
+        console.log('Success', data);
+        Toast.success(`${shift.Outbound} - ${shift.Inbound} on ${shift.Date} submitted successfully!`);
+      })
+      .catch(error => {
+        console.log(error);
+        Toast.error(`${shift.Outbound} - ${shift.Inbound} on ${shift.Date} submission failed`)
+      });
+    });
+  };
 
   return (
     <>
       <View style={styles.InlineForm}>
         <View>
-          <TextInput style={styles.Email} placeholder="Email"></TextInput>
+          <TextInput style={styles.Email} placeholder="Email" onChangeText={(value) => onChangeEmail(value)} value={email} />
         </View>
 
         <ScrollView horizontal>
-          <View style={{flexDirection: 'column'}}>
-            <View style={{flexDirection: 'row', margin: 0}}>
-              <View style={styles.SHIFT}><Text style={{fontWeight: 'bold'}}>SHIFT</Text></View>
-              <View style={styles.Date}><Text style={{fontWeight: 'bold'}}>Date</Text></View>
-              <View style={styles.Outbound}><Text style={{fontWeight: 'bold'}}>Outbound</Text></View>
-              <View style={styles.Inbound}><Text style={{fontWeight: 'bold'}}>Inbound</Text></View>
-              <View style={styles.Overnight}><Text style={{fontWeight: 'bold'}}>Overnight</Text></View>
-              <View style={styles.FIRST}><Text style={{fontWeight: 'bold'}}>FIRST</Text></View>
-              <View style={styles.BAR}><Text style={{fontWeight: 'bold'}}>BAR</Text></View>
-              <View style={styles.PURSER}><Text style={{fontWeight: 'bold'}}>PURSER</Text></View>
-              <View style={styles.FOR}><Text style={{fontWeight: 'bold'}}>FOR</Text></View>
-              <View style={styles.Early}><Text style={{fontWeight: 'bold'}}>Early</Text></View>
-              <View style={styles.Late}><Text style={{fontWeight: 'bold'}}>Late</Text></View>
-              <View style={styles.LTA}><Text style={{fontWeight: 'bold'}}>LTA</Text></View>
-              <View style={styles.DO}><Text style={{fontWeight: 'bold'}}>D.O.</Text></View>
-            </View>
-              {/* {shifts.map((shift, index) => (
-                <>
-                  <View key={index} style={{flexDirection: 'row', margin: 0}}>
-                    <View style={styles.SHIFT}>
-                      <TouchableOpacity style={styles.AddButton}><Text style={{color: 'white', alignSelf: 'center'}}>+</Text></TouchableOpacity>
-                      <TouchableOpacity style={styles.DeleteButton}><Text style={{color: 'black', alignSelf: 'center'}}>-</Text></TouchableOpacity>
-                    </View>
-                    <View style={styles.Date}></View>
-                    <View style={styles.Outbound}></View>
-                    <View style={styles.Inbound}></View>
-                    <View style={styles.Overnight}></View>
-                    <View style={styles.FIRST}></View>
-                    <View style={styles.BAR}></View>
-                    <View style={styles.PURSER}></View>
-                    <View style={styles.FOR}></View>
-                    <View style={styles.Early}></View>
-                    <View style={styles.Late}></View>
-                    <View style={styles.LTA}></View>
-                    <View style={styles.DO}></View>
+          <ScrollView>
+            <View style={{flexDirection: 'column'}}>
+              <View style={{flexDirection: 'row', margin: 0}}>
+                <View style={styles.SHIFT}><Text style={{fontWeight: 'bold'}}>SHIFT</Text></View>
+                <View style={styles.Date}><Text style={{fontWeight: 'bold'}}>Date</Text></View>
+                <View style={styles.Outbound}><Text style={{fontWeight: 'bold'}}>Outbound</Text></View>
+                <View style={styles.Inbound}><Text style={{fontWeight: 'bold'}}>Inbound</Text></View>
+                <View style={styles.Overnight}><Text style={{fontWeight: 'bold'}}>Overnight</Text></View>
+                <View style={styles.FIRST}><Text style={{fontWeight: 'bold'}}>FIRST</Text></View>
+                <View style={styles.BAR}><Text style={{fontWeight: 'bold'}}>BAR</Text></View>
+                <View style={styles.PURSER}><Text style={{fontWeight: 'bold'}}>PURSER</Text></View>
+                <View style={styles.FOR}><Text style={{fontWeight: 'bold'}}>FOR</Text></View>
+                <View style={styles.Early}><Text style={{fontWeight: 'bold'}}>Early</Text></View>
+                <View style={styles.Late}><Text style={{fontWeight: 'bold'}}>Late</Text></View>
+                <View style={styles.LTA}><Text style={{fontWeight: 'bold'}}>LTA</Text></View>
+                <View style={styles.DO}><Text style={{fontWeight: 'bold'}}>D.O.</Text></View>
+              </View>
+
+              {shifts.map((shift, index) => (
+                <View key={index} style={{flexDirection: 'row', margin: 0}}>
+                  <View style={styles.SHIFT}>
+                    <TouchableOpacity style={styles.AddButton} onPress={addShift}><Text style={{color: 'white', fontSize: 9}}>+</Text></TouchableOpacity>
+                    <TouchableOpacity style={styles.DeleteButton} onPress={() => deleteShift(index)}><Text style={{color: 'white', fontSize: 9}}>-</Text></TouchableOpacity>
                   </View>
-                </>
-              ))} */}
-          </View>
+                  <View style={styles.Date}>
+                    <TouchableOpacity onPress={openDatePicker}>
+                      <Text>{selectedDate.toDateString()}</Text>
+                    </TouchableOpacity>
+                      {showDatePicker && (
+                        <DateTimePicker
+                          value={selectedDate}
+                          mode="date"
+                          display="spinner"
+                          onChange={handleDateChange}
+                        />
+                      )}
+                  </View>
+                  <View style={styles.Outbound}><TextInput onChangeText={(value) => handleChange(index, 'Outbound', value)} value={shift.Outbound} placeholder="Outbound" keyboardType="numeric" /></View>
+                  <View style={styles.Inbound}><TextInput onChangeText={(value) => handleChange(index, 'Inbound', value)} value={shift.Inbound} placeholder="Inbound" keyboardType="numeric" /></View>
+                  <View style={styles.Overnight}><Switch onValueChange={ovSwitch(index)} value={shift.isOvernight}></Switch></View>
+                  <View style={styles.FIRST}>
+                    <CheckBox 
+                      value={selectedPosition === 'FIRST'} 
+                      onValueChange={() => {
+                        setSelectedPosition('FIRST');
+                        handleChange(index, 'Position', 'FIRST')
+                      }} 
+                    />
+                  </View>
+                  <View style={styles.BAR}>
+                    <CheckBox 
+                      value={selectedPosition === 'BAR'} 
+                      onValueChange={() => {
+                        setSelectedPosition('BAR');
+                        handleChange(index, 'Position', 'BAR')
+                      }} 
+                    />
+                  </View>
+                  <View style={styles.PURSER}>
+                    <CheckBox 
+                      value={selectedPosition === 'PURSER'} 
+                      onValueChange={() => {
+                        setSelectedPosition('PURSER');
+                        handleChange(index, 'Position', 'PURSER')
+                      }} 
+                    />
+                  </View>
+                  <View style={styles.FOR}></View>
+                  <View style={styles.Early}><CheckBox value={shift.Early} onValueChange={() => handleChange(index, 'Early', !shift.Early)}></CheckBox></View>
+                  <View style={styles.Late}><CheckBox value={shift.Late} onValueChange={() => handleChange(index, 'Late', !shift.Late)}></CheckBox></View>
+                  <View style={styles.LTA}><CheckBox value={shift.LTA} onValueChange={() => handleChange(index, 'LTA', !shift.LTA)}></CheckBox></View>
+                  <View style={styles.DO}><CheckBox value={shift.DO} onValueChange={() => handleChange(index, 'DO', !shift.DO)}></CheckBox></View>
+                </View>
+              ))}
+
+            </View>
+          </ScrollView>
         </ScrollView>
 
         <View style={{flexDirection: 'column', margin: 2, justifyContent: 'center'}}>
-          <TextInput style={styles.Note} placeholder="Note"></TextInput>
+          <TextInput style={styles.Note} placeholder="Note" onChangeText={(value) => onChangeNote(value)} value={note}></TextInput>
           <View style={{flexDirection: 'row', margin: 2, justifyContent: 'center'}}>
             <TouchableOpacity style={styles.Submit_Button}><Text style={{color:'#fff'}} onPress={handleSubmit}>Submit</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.SwapForm_Button}><Text style={{color:'#fff'}} onPress={handleSwapFormPress}>Swap Form</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.Roster_Button}><Text style={{color:'#fff'}} onPress={handleRosterPress}>Roster</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.Link1_Button}><Text style={{color:'#fff'}} onPress={handleLink1Press}>Link 1</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.Link2_Button}><Text style={{color:'#fff'}} onPress={handleLink2Press}>Link 2</Text></TouchableOpacity>
           </View>
         </View>
       </View>
@@ -102,16 +239,20 @@ const styles = StyleSheet.create({
   AddButton: {
     backgroundColor: 'blue',
     borderRadius: 3,
-    borderStyle: 'none',
     height: 12,
     width: 12,
+    margin: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   DeleteButton: {
     backgroundColor: 'red',
     borderRadius: 3,
-    borderStyle: 'none',
     height: 12,
     width: 12,
+    margin: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   Submit_Button: {
     backgroundColor: 'blue',
@@ -122,7 +263,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  SwapForm_Button: {
+  Link1_Button: {
     backgroundColor: '#4fd009',
     borderRadius: 4,
     height: 25,
@@ -131,7 +272,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  Roster_Button: {
+  Link2_Button: {
     backgroundColor: '#4fd009',
     borderRadius: 4,
     height: 25,
@@ -140,7 +281,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  SHIFT: {width: 60, height: 20, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: '#ccc'},
+  SHIFT: {width: 60, height: 20, flexDirection: 'row', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: '#ccc'},
   Date: {width: 80, height: 20, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: '#ccc'},
   Outbound: {width: 80, height: 20, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: '#ccc'},
   Inbound: {width: 80, height: 20, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: '#ccc'},
